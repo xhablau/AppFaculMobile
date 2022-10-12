432ef78e456c85b44c55a1bf9a093d7e05baf9ab
xhablau <96922185+xhablau@users.noreply.github.com>
2022-10-11 22:20:19 -0300

    alterações

--- AppFacul2/app/src/main/java/com/example/appfacul/MainActivity.kt
+++ AppFacul2/app/src/main/java/com/example/appfacul/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.appfacul
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import com.example.appfacul.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+        binding.btnAccess.setOnClickListener{
+            val idUser = binding.idUser.text.toString()
+            val passwordUser = binding.passwordUser.text.toString()
+
+            if (idUser.isEmpty() || passwordUser.isEmpty()) {
+                Toast.makeText(this,"Preencha todos os campos!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this,"Não!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
