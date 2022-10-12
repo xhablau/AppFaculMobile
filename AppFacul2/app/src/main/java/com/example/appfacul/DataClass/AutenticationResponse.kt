c36d36ead95de59bdb15e3c28623f88fc408d849
PhRn18 <paulinhohenriqueneves@hotmail.com>
2022-10-12 18:13:28 -0300

    conexoes

---
+++ AppFacul2/app/src/main/java/com/example/appfacul/DataClass/AutenticationResponse.kt
@@ -0,0 +1,9 @@
+package com.example.appfacul.DataClass
+
+data class AutenticationResponse(
+    val login:String,
+    val dialect:String,
+    val email:String,
+    val username:String,
+    val authenticated:Boolean
+)
--- AppFacul2/app/src/main/java/com/example/appfacul/MainActivity.kt
+++ AppFacul2/app/src/main/java/com/example/appfacul/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.appfacul
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appfacul.LoginControler.LoginControler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        LoginControler().FormLoginControler()
     }
 }
