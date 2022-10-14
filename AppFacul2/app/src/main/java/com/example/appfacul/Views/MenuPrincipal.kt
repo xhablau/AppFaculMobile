0c8721f7f9e17ec6452bfe6aba3809192a1d9dc2
xhablau <96922185+xhablau@users.noreply.github.com>
2022-10-14 19:55:27 -0300

    menu 22

--- AppFacul2/app/src/main/java/com/example/appfacul/Views/MenuPrincipal.kt
+++ AppFacul2/app/src/main/java/com/example/appfacul/Views/MenuPrincipal.kt
@@ -3,8 +3,15 @@ package com.example.appfacul.Views
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.appfacul.GlobalClass
 import com.example.appfacul.R
 import com.example.appfacul.StartNewActivity.StartNewActivity
@@ -13,10 +20,35 @@ import java.util.*
 
 
 class MenuPrincipal : AppCompatActivity() {
+    lateinit var drawerOpen:ImageView
+
+    lateinit var navigationDrawer:NavigationView
+
+    lateinit var drawerLayout:DrawerLayout
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_principal)
         supportActionBar?.hide()
+
+        drawerOpen=findViewById(R.id.drawer_open)
+
+        navigationDrawer=findViewById(R.id.navigation_drawer)
+
+        drawerLayout=findViewById(R.id.drawer_layout)
+
+        drawerOpen.setOnClickListener {
+
+            drawerLayout.openDrawer(GravityCompat.START)
+
+        }
+
+        var navController:NavController=Navigation.findNavController(this,R.id.fragment)
+
+        NavigationUI.setupWithNavController(navigationDrawer,navController)
 
         val globalClass= GlobalClass()
         val textViewName = findViewById<TextView>(R.id.menuPrincipalHeader)
