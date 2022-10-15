70523988acce461d37e93a87abebd0499bd47bc3
xhablau <96922185+xhablau@users.noreply.github.com>
2022-10-15 17:31:06 -0300

    menu 22

--- AppFacul2/app/src/main/java/com/example/appfacul/Views/MenuPrincipal.kt
+++ AppFacul2/app/src/main/java/com/example/appfacul/Views/MenuPrincipal.kt
@@ -51,18 +51,18 @@ class MenuPrincipal : AppCompatActivity() {
         NavigationUI.setupWithNavController(navigationDrawer,navController)
 
         val globalClass= GlobalClass()
-        val textViewName = findViewById<TextView>(R.id.menuPrincipalHeader)
-        val textViewTime = findViewById<TextView>(R.id.menuPrincipalHeaderTime)
-        textViewName.text = getString(R.string.welcome_messages,globalClass.globalUserName)
+       // val textViewName = findViewById<TextView>(R.id.menuPrincipalHeader)
+       // val textViewTime = findViewById<TextView>(R.id.menuPrincipalHeaderTime)
+       // textViewName.text = getString(R.string.welcome_messages,globalClass.globalUserName)
 
         val date = Calendar.getInstance().time
         val formatter = SimpleDateFormat("dd/MM/yyyy",Locale.ENGLISH) //or use getDateInstance()
-        textViewTime.text=formatter.format(date)
+      //  textViewTime.text=formatter.format(date)
 
-        val buttonCadastroAcademico:Button = findViewById(R.id.buttonCadastroAcademico)
-        buttonCadastroAcademico.setOnClickListener{
-            StartNewActivity(this).InitializeActivityCadastroAcademico(CadastroAcademico::class.java)
-        }
+      // val buttonCadastroAcademico:Button = findViewById(R.id.buttonCadastroAcademico)
+      //  buttonCadastroAcademico.setOnClickListener{
+       //     StartNewActivity(this).InitializeActivityCadastroAcademico(CadastroAcademico::class.java)
+       // }
         
     }
 }
