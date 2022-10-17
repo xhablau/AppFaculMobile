c7cbd342aa7a24d94b06f6a814360cb5cb625d20
xhablau <96922185+xhablau@users.noreply.github.com>
2022-10-17 20:06:39 -0300

    menu 22

--- AppFacul2/app/src/main/java/com/example/appfacul/Views/MenuPrincipal.kt
+++ AppFacul2/app/src/main/java/com/example/appfacul/Views/MenuPrincipal.kt
@@ -52,12 +52,12 @@ class MenuPrincipal : AppCompatActivity() {
 
         val globalClass= GlobalClass()
        // val textViewName = findViewById<TextView>(R.id.menuPrincipalHeader)
-       // val textViewTime = findViewById<TextView>(R.id.menuPrincipalHeaderTime)
+        val textViewTime = findViewById<TextView>(R.id.menuPrincipalHeaderTime)
        // textViewName.text = getString(R.string.welcome_messages,globalClass.globalUserName)
 
         val date = Calendar.getInstance().time
         val formatter = SimpleDateFormat("dd/MM/yyyy",Locale.ENGLISH) //or use getDateInstance()
-      //  textViewTime.text=formatter.format(date)
+        textViewTime.text=formatter.format(date)
 
       // val buttonCadastroAcademico:Button = findViewById(R.id.buttonCadastroAcademico)
       //  buttonCadastroAcademico.setOnClickListener{
