f3cfbd70a39f7c8856bc0e75a700f13bd38f9aad
xhablau <96922185+xhablau@users.noreply.github.com>
2022-10-17 22:56:54 -0300

    menu 22

--- AppFacul2/app/src/main/java/com/example/appfacul/Views/MenuPrincipal.kt
+++ AppFacul2/app/src/main/java/com/example/appfacul/Views/MenuPrincipal.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -15,6 +16,8 @@ import androidx.navigation.ui.NavigationUI
 import com.example.appfacul.GlobalClass
 import com.example.appfacul.R
 import com.example.appfacul.StartNewActivity.StartNewActivity
+import com.example.appfacul.databinding.ActivityMainBinding
+import com.example.appfacul.databinding.FragmentHomeBinding
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -26,7 +29,7 @@ class MenuPrincipal : AppCompatActivity() {
 
     lateinit var drawerLayout:DrawerLayout
 
-
+    private lateinit var binding: FragmentHomeBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,19 +53,30 @@ class MenuPrincipal : AppCompatActivity() {
 
         NavigationUI.setupWithNavController(navigationDrawer,navController)
 
-        val globalClass= GlobalClass()
-       // val textViewName = findViewById<TextView>(R.id.menuPrincipalHeader)
-        val textViewTime = findViewById<TextView>(R.id.menuPrincipalHeaderTime)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
+
+        //val teste = binding.menuPrincipalHeaderTime
+
+
+       // teste.text= setTex
+       // val globalClass= GlobalClass()
+        // val textViewName = findViewById<TextView>(R.id.menuPrincipalHeader)
+       // val textViewTime = findViewById<TextView>(R.id.menuPrincipalHeaderTime)
        // textViewName.text = getString(R.string.welcome_messages,globalClass.globalUserName)
 
-        val date = Calendar.getInstance().time
-        val formatter = SimpleDateFormat("dd/MM/yyyy",Locale.ENGLISH) //or use getDateInstance()
-        textViewTime.text=formatter.format(date)
+        //val date = Calendar.getInstance().time
+        //val formatter = SimpleDateFormat("dd/MM/yyyy",Locale.ENGLISH) //or use getDateInstance()
+        //teste.text=formatter.format(date)
+
+
 
       // val buttonCadastroAcademico:Button = findViewById(R.id.buttonCadastroAcademico)
-      //  buttonCadastroAcademico.setOnClickListener{
-       //     StartNewActivity(this).InitializeActivityCadastroAcademico(CadastroAcademico::class.java)
-       // }
+       // buttonCadastroAcademico.setOnClickListener{
+        //    StartNewActivity(this).InitializeActivityCadastroAcademico(CadastroAcademico::class.java)
+        //}
         
     }
 }
+
+
